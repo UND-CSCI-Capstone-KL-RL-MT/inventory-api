b8c1a58ec36415c91d6fb15545f5bcdd63afd463
Ben Alman <benjamin.alman@und.edu>
2015-10-10 11:55:07 -0500

    Added in handlers for get users and get inventory requests.

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -1,6 +1,6 @@
 <?php
 	global $mysqli;
-	$mysqli = new mysqli("mysqldev.aero.und.edu", "balman", "ks945tp", "balman");
+	$mysqli = new mysqli("mysqldev.aero.und.edu", "balman", "ks945tp", "kerlindb");
 	if ($mysqli->connect_error) {
 		die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
 	}
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+include("includes/connect.inc.php");
+
+if (isset($_GET["users"])) {
+	$query = "SELECT user_id, username FROM users WHERE 1;";
+
+	if ($stmt = $mysqli->prepare($query)) {
+	
+		$stmt->execute();
+		$stmt->store_result();
+	    	$stmt->bind_result($user_id, $user_name);
+		$result_array = array();
+	    	while($stmt->fetch()) {
+	    		array_push($result_array, (object) array("user_id" => $user_id, "username" => $user_name));
+	    	}
+		$stmt->free_result();
+		$stmt->close();
+		echo json_encode($result_array);
+	}
+} else if (isset($_GET["inventory"])) {
+	$query = "SELECT item_id, item_desc, item_loc FROM inventory WHERE 1;";
+
+	if ($stmt = $mysqli->prepare($query)) {
+	
+		$stmt->execute();
+		$stmt->store_result();
+	    	$stmt->bind_result($item_id, $item_desc, $item_loc);
+		$result_array = array();
+	    	while($stmt->fetch()) {
+	    		array_push($result_array, (object) array("item_id" => $item_id, "item_description" => $item_desc, "item_location" => $item_loc));
+	    	}
+		$stmt->free_result();
+		$stmt->close();
+		echo json_encode($result_array);
+	}
+}
+?>
