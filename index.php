d6b2701652515d8bdb04f24f183fc09df51f54a8
Ben Alman <hi@benalman.me>
2015-10-24 12:35:08 -0500

    Added handling of delete item request.

--- index.php
+++ index.php
@@ -118,7 +118,22 @@ if (isset($_GET["users"])) {
     /* Handle POST request deleting an item */
     else if ($request_type == "remove_item") {
         
-        // remove item handler TBA
+        // Collect information from the POST request
+        $item_id = $request->itemID; // the M-tag for the item
+        
+        // Construct query
+        $query = "DELETE FROM inventory WHERE item_id = ?";
+        
+        // Prepare the query, bind the ID, and execute the command
+        if ($stmt = $mysqli->prepare($query)) {
+            $stmt->bind_param("s", item_id);
+            $stmt->execute;
+            $stmt->close();
+            echo "{\"result\":\"success\",\"message\":\"Item with ID " . $item_id . " removed successfully.\"}";
+        } else {
+            // if we weren't able to prepare the query, die and tell user.
+            die("{\"result\":\"error\",\"message\":\"Unable to prepare your request. (3)\"}");
+        }
         
     }
     
