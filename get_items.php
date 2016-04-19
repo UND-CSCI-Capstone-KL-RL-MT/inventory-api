7b20f14eb1a1f787deec6e7c423606a9b38fe8e7
Ben Alman <benjamin.alman@und.edu>
2016-04-19 12:46:39 -0500

    Added ability to filter in get_items function, minor fixes in remove_item and update_item.

--- get_items.php
+++ get_items.php
@@ -2,21 +2,45 @@
 
 include('includes/connect.inc.php');
 
-$query = "SELECT item_id, item_desc, item_building, item_loc FROM inventory WHERE 1";
+if (isset($_GET["filter"])) {
+	
+	$filter = $_GET["filter"];
+	$query = "SELECT item_id, item_desc, item_building, item_loc FROM inventory WHERE item_desc LIKE '%{$filter}%'";
+	
+	if ($stmt = $mysqli->prepare($query)) {
 
-if ($stmt = $mysqli->prepare($query)) {
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->bind_result($item_id, $item_desc, $item_building, $item_loc);
+		$result_array = array();
+		while($stmt->fetch()) {
+			array_push($result_array, (object) array("item_id" => $item_id, "item_description" => $item_desc, "item_building" => $item_building, "item_location" => $item_loc));
+		}
+		$stmt->free_result();
+		$stmt->close();
+		echo json_encode($result_array);
 
-	$stmt->execute();
-	$stmt->store_result();
-	$stmt->bind_result($item_id, $item_desc, $item_building, $item_loc);
-	$result_array = array();
-	while($stmt->fetch()) {
-		array_push($result_array, (object) array("item_id" => $item_id, "item_description" => $item_desc, "item_building" => $item_building, "item_location" => $item_loc));
 	}
-	$stmt->free_result();
-	$stmt->close();
-	echo json_encode($result_array);
+	
+} else {
+	
+	$query = "SELECT item_id, item_desc, item_building, item_loc FROM inventory WHERE 1";
 
+	if ($stmt = $mysqli->prepare($query)) {
+
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->bind_result($item_id, $item_desc, $item_building, $item_loc);
+		$result_array = array();
+		while($stmt->fetch()) {
+			array_push($result_array, (object) array("item_id" => $item_id, "item_description" => $item_desc, "item_building" => $item_building, "item_location" => $item_loc));
+		}
+		$stmt->free_result();
+		$stmt->close();
+		echo json_encode($result_array);
+
+	}
+	
 }
 
 ?>
--- update_item.php
+++ update_item.php
@@ -5,7 +5,7 @@ include('includes/connect.inc.php');
 // Collect information from POST request
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
-$item_id = $request->id; // the M-tag for the item
+$item_id = $request->item_id; // the M-tag for the item
 $item_desc = $request->item_description; // the description of the item
 $item_loc = $request->item_location; // item location (room #)
 $item_building = $request->item_building;
@@ -15,7 +15,7 @@ $query = "UPDATE inventory SET item_desc = ?, item_loc = ?, item_building = ? WH
 
 // Prepare the query, bind the description/location/ID, and execute.
 if ($stmt = $mysqli->prepare($query)) {
-	$stmt->bind_param("siss", $item_desc, $item_loc, $item_id, $item_building);
+	$stmt->bind_param("siss", $item_desc, $item_loc, $item_building, $item_id);
 	$stmt->execute();
 	$stmt->close();
 	echo "{\"result\":\"success\",\"message\":\"Item information for " . $item_id . " updated successfully.\"}";
